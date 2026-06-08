11d3ce17e7f2e84fe608f9c9f39e05a1ca46e3af
Fengli5355 <fengli5355@gmail.com>
2026-06-08 14:35:39 +0800

    [Feat] Implement AsuStore (#979)
    Add implmentation of AsuStore.
    
    store/asu/cc/asu_store.cc temprary modifications.
    
    store/test/case/asu/asu_store_test.cc temprary tests.
    
    ---------
    
    Co-authored-by: Qiong Wu <wq674452797@gmail.com>

--- ucm/store/pipeline/connector.py
+++ ucm/store/pipeline/connector.py
@@ -362,12 +362,20 @@ def _yuanrong_posix_pipeline_builder(
     _stack_yuanrong_store(config, pipeline)
 
 
+def _asu_pipeline_builder(
+    config: Dict[str, object], pipeline: ucmpipelinestore.PipelineStore
+):
+    store_dir = Path(__file__).resolve().parent.parent
+    pipeline.Stack("Asu", str(store_dir / "asu/libasustore.so"), config)
+
+
 UcmPipelineStoreBuilder.register("Cache|Ds3fs", _cache_ds3fs_pipeline_builder)
 UcmPipelineStoreBuilder.register("Cache|Empty", _cache_empty_pipeline_builder)
 UcmPipelineStoreBuilder.register("Cache|Posix", _cache_posix_pipeline_builder)
 UcmPipelineStoreBuilder.register("Empty", _empty_pipeline_builder)
 UcmPipelineStoreBuilder.register("Fake", _fake_pipeline_builder)
 UcmPipelineStoreBuilder.register("Posix", _posix_pipeline_builder)
+UcmPipelineStoreBuilder.register("ASU", _asu_pipeline_builder)
 UcmPipelineStoreBuilder.register(
     "Cache|Compress|Posix", _build_cache_compress_posix_pipeline
 )
