125ae11d375945cf40e0b960e627338efa910863
yuanzhg078 <939526371@qq.com>
2026-07-10 12:40:32 +0800

    [Fix]: Save KV cache to connector in layerwise MLA scenarios

--- ucm/integration/vllm/patch/v0191/vllm_ascend/pc_ascend_patch.py
+++ ucm/integration/vllm/patch/v0191/vllm_ascend/pc_ascend_patch.py
@@ -1,9 +1,18 @@
 import ucm.integration.vllm.patch.scheduler_metrics_patch  # noqa: F401
 import ucm.integration.vllm.patch.v0191.vllm_ascend.ucm_connector_patch  # noqa: F401
-from ucm.integration.vllm.patch.utils import when_imported
+from ucm.integration.vllm.patch.utils import patch_or_inject, when_imported
 from ucm.logger import init_logger
 
 logger = init_logger(__name__)
+
+
+@when_imported("vllm_ascend.attention.mla_v1")
+def patch_mla_v1(mod):
+    logger.info(f"Patched {mod} MLA forward")
+
+    from ucm.integration.vllm.patch.v0191.vllm_ascend.pc.attention import mla_v1
+
+    patch_or_inject(mod.AscendMLAImpl, "forward", mla_v1.AscendMLAImpl.forward)
 
 
 @when_imported("vllm_ascend.patch.platform.patch_mamba_config")
