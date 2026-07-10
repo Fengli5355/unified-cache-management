6627f518c7569993a88840b06466ea45262954fc
yuanzhg078 <939526371@qq.com>
2026-07-10 11:18:08 +0800

    [Chore]: Sync patch from develop commit 22f014567981b5218c055bf4987f56e1fc9c73b5

--- ucm/integration/vllm/patch/v0191/vllm/pc_patch.py
+++ ucm/integration/vllm/patch/v0191/vllm/pc_patch.py
@@ -4,19 +4,6 @@ from ucm.logger import init_logger
 logger = init_logger(__name__)
 
 
-@when_imported("vllm.v1.metrics.stats")
-def patch_stats(mod):
-    logger.debug(f"Patched {mod} called")
-
-    from ucm.integration.vllm.patch.v0180.vllm.pc.metrics import stats
-
-    patch_or_inject(
-        mod.PromptTokenStats,
-        "update_from_output",
-        stats.update_from_output,
-    )
-
-
 @when_imported("vllm.config.vllm")
 def patch_vllm_config(mod):
     logger.debug(f"Patched {mod} called")
