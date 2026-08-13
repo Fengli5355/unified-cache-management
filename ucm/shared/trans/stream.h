5f324c602be8b41c3bf37818e2d77f59a761f9d4
pengyao <pyxyzc@gmail.com>
2026-08-13 09:54:30 +0800

    [Feat] Add DeviceToDevice copy infertace for trans (#1224)
    Add DeviceToDevice copy infertace for trans :)

--- ucm/shared/trans/stream.h
+++ ucm/shared/trans/stream.h
@@ -79,6 +79,18 @@ public:
         }
         return Status::OK();
     }
+
+    virtual Status DeviceToDevice(void* source, void* destination, size_t size) = 0;
+    virtual Status DeviceToDevice(void* source[], void* destination[], size_t size,
+                                  size_t number) = 0;
+    virtual Status DeviceToDevice(void* source[], void* destination, size_t size,
+                                  size_t number) = 0;
+    virtual Status DeviceToDeviceAsync(void* source, void* destination, size_t size) = 0;
+    virtual Status DeviceToDeviceAsync(void* source[], void* destination[], size_t size,
+                                       size_t number) = 0;
+    virtual Status DeviceToDeviceAsync(void* source[], void* destination, size_t size,
+                                       size_t number) = 0;
+
     virtual Status AppendCallback(std::function<void(bool)> cb) = 0;
     virtual Status Synchronized() = 0;
     virtual Status WaitEvent(void* event) = 0;
